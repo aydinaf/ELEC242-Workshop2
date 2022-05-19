f71d1a688f81c73350e88646e633ac958ba558db
kav0321 <52637046+kav0321@users.noreply.github.com>
2022-05-19 17:05:22 -0400

    final code

--- question5.m
+++ question5.m
@@ -8,12 +8,21 @@ c=w*j+10;
 d=w*j+1000;
 e=w*j+10000;
 x1=(a.*b)./(c.*d.*e);
+
+%magnitude vs frequency
 subplot(1,4,1);
+
 r=20*log10(abs(x1));
+
 semilogx(w,r);
+
+xlabel('frequency')
+ylabel('magnitude')
+%phase vs frequency
 subplot(1,4,2);
 semilogx(w,angle(x1));
-max_x1=max(r);
+
+
 %low att
 i=0.5; 
 low_att=0;
@@ -25,9 +34,13 @@ c=i*j+10;
 d=i*j+1000;
 e=i*j+10000;
 x1(n)=(a.*b)./(c.*d.*e);
-x1=20*log10(abs(x1));
-if(x1(n)<=max_x1 && x1(n)>=(max_x1-20))
+r1=20*log10(abs(x1));
+max_x1=max(r1);
+if(r1(n)<=max_x1 && r1(n)>=(max_x1-20))
+    
     low_att(n)=i;
+else
+    low_att(n)=0;
 end
 i=i+5;
 n=n+1;
@@ -52,16 +65,18 @@ c=i*j+10;
 d=i*j+1000;
 e=i*j+10000;
 x1(n)=(a.*b)./(c.*d.*e);
-x1=20*log10(abs(x1));
-if(x1(n)<=max_x1-30 && x1(n)>=(min_x1))
+r1=20*log10(abs(x1));
+max_x1=max(r1);
+if(r1(n)<=max_x1-30 && r1(n)>=(min_x1))
     high_att(n)=i;
 else 
     high_att(n)=0;
+
 end
 
 i=i+5;
 n=n+1;
 end
-display(high_att)
+
 subplot(1,4,4);
 semilogx(high_att,r)
