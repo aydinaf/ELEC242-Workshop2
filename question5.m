282574800a09a0d3f2e7b7c89992395c8d065eee
Aydin Azari Farhad <33768239+aydinaf@users.noreply.github.com>
2022-05-19 16:44:09 -0400

    Commit 3 - Graphs Look Fine

--- question5.m
+++ question5.m
@@ -13,8 +13,7 @@ r=20*log10(abs(x1));
 semilogx(w,r);
 subplot(1,4,2);
 semilogx(w,angle(x1));
-
-
+max_x1=max(r);
 %low att
 i=0.5; 
 low_att=0;
@@ -26,12 +25,9 @@ c=i*j+10;
 d=i*j+1000;
 e=i*j+10000;
 x1(n)=(a.*b)./(c.*d.*e);
-x1=20*log(abs(x1));
-max_x1=max(x1);
+x1=20*log10(abs(x1));
 if(x1(n)<=max_x1 && x1(n)>=(max_x1-20))
     low_att(n)=i;
-else
-    low_att(n)=0;
 end
 i=i+5;
 n=n+1;
@@ -56,18 +52,16 @@ c=i*j+10;
 d=i*j+1000;
 e=i*j+10000;
 x1(n)=(a.*b)./(c.*d.*e);
-x1=20*log(abs(x1));
-max_x1=max(x1);
+x1=20*log10(abs(x1));
 if(x1(n)<=max_x1-30 && x1(n)>=(min_x1))
     high_att(n)=i;
 else 
     high_att(n)=0;
-
 end
 
 i=i+5;
 n=n+1;
 end
-
+display(high_att)
 subplot(1,4,4);
 semilogx(high_att,r)
