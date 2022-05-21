a133bfabb690a4f0e80ffcd27180e85417ff9204
Aydin Azari Farhad <33768239+aydinaf@users.noreply.github.com>
2022-05-21 17:57:18 -0400

    Parts A,B, and C complete
    Needs full name and student ID on line 4. @kav0321 
    Needs a final revision to verify answers.

--- question5.m
+++ question5.m
@@ -1,6 +1,10 @@
-
+% ELEC 242 - Summer 2022 - Workshop 2 Question 5
+% Matlab solution to question 5:
+% Aydin Azari Farhad - 40063330
+% Kavish ##########
+%---------------------------------
 % Part a)
-%part a
+
 w=0.5:5:3000;
 a=w*j+100;
 b=w*j+200;
@@ -15,26 +19,26 @@ subplot(3,4,1);
 r=20*log10(abs(x1));
 
 semilogx(w,r);
-title('Magnitude vs Frequency')
+
 xlabel('frequency')
 ylabel('magnitude')
 %phase vs frequency
 subplot(3,4,2);
 semilogx(w,angle(x1));
 
-title('Angle vs Frequency')
-xlabel('frequency')
-ylabel('angle')
-max_x1=max(r1);
 %low att
 i=0.5; 
 low_att=0;
 n=1;
 while(i<=3000)
-
-x1(n)=(a(n)*b(n))./(c(n)*d(n)*e(n));
-r1(n)=20*log10(abs(x1(n)));
-
+a=i*j+100;
+b=i*j+200;
+c=i*j+10;
+d=i*j+1000;
+e=i*j+10000;
+x1(n)=(a.*b)./(c.*d.*e);
+r1=20*log10(abs(x1));
+max_x1=max(r1);
 if(r1(n)<=max_x1 && r1(n)>=(max_x1-20))
     
     low_att(n)=i;
@@ -48,24 +52,20 @@ end
 subplot(3,4,3);
 semilogx(low_att,r)
 
-title('Low Attinuation')
-xlabel('frequency')
-ylabel('magnitude')
-
-
-
-
 %high att
 i=0.5; 
 high_att=0;
 n=1;
 min_x1=min(r);
-max_x1=max(r1);
 while(i<=3000)
-
-x1(n)=(a(n)*b(n))./(c(n)*d(n)*e(n));
-r1(n)=20*log10(abs(x1(n)));
-
+a=i*j+100;
+b=i*j+200;
+c=i*j+10;
+d=i*j+1000;
+e=i*j+10000;
+x1(n)=(a.*b)./(c.*d.*e);
+r1=20*log10(abs(x1));
+max_x1=max(r1);
 if(r1(n)<=max_x1-30 && r1(n)>=(min_x1))
     high_att(n)=i;
 else 
@@ -79,17 +79,9 @@ end
 
 subplot(3,4,4);
 semilogx(high_att,r)
-title('High Attinuation')
-xlabel('frequency')
-ylabel('magnitude')
 
-
-%partb
-
-% Matlab solution to question 5:
-% Part a)
-%part a
-
+%--------------------------------
+% Part B)
 a2=w*j;
 b2=40*w*j+40000;
 
@@ -101,7 +93,6 @@ end
 
 x2= c2./(a2.^2+b2);
 %magnitude vs frequency
-
 
 r2=20*log10(abs(x2));
 subplot(3,4,5);
@@ -117,8 +108,6 @@ title('Angle vs Frequency')
 xlabel('frequency')
 ylabel('angle')
 
-
-
 %low att
 k=0.5; 
 low_att2=0;
@@ -126,7 +115,6 @@ n=1;
 max_x2=max(r2);
 min_x2=min(r2);
 while(k<=3000)
-
 
 x2(n)= c2(n)/(a2(n)^2+b2(n));
 r22(n)=20*log10(abs(x2(n)));
@@ -148,14 +136,10 @@ title('Low Attinuation')
 xlabel('frequency')
 ylabel('magnitude')
 
-
-
-
 k=0.5; 
 high_att2=0;
 n=1;
 while(k<=3000)
-
 
 x2(n)= c2(n)/(a2(n)^2+b2(n));
 r22(n)=20*log10(abs(x2(n)));
@@ -177,3 +161,85 @@ title('high Attinuation')
 xlabel('frequency')
 ylabel('magnitude')
 
+%--------------------------------
+% Part C)
+a2=w*j;
+b2=160*w*j+1000;
+
+%since it will be a matrix opperation, there should be the same size as a2
+%and b2 matrices
+for i=1:600
+c2(i)=10000;
+end
+
+x2= c2./(a2.^2+b2);
+%magnitude vs frequency
+
+r2=20*log10(abs(x2));
+subplot(3,4,9);
+semilogx(w,r2);
+title('Magnitude vs Frequency')
+xlabel('frequency')
+ylabel('magnitude')
+%phase vs frequency
+subplot(3,4,10);
+semilogx(w,angle(x2));
+
+title('Angle vs Frequency')
+xlabel('frequency')
+ylabel('angle')
+
+%low att
+k=0.5; 
+low_att2=0;
+n=1;
+max_x2=max(r2);
+min_x2=min(r2);
+while(k<=3000)
+
+x2(n)= c2(n)/(a2(n)^2+b2(n));
+r22(n)=20*log10(abs(x2(n)));
+
+if(r22(n)<=max_x2 && r22(n)>=(max_x2-20))
+    
+    low_att2(n)=k;
+else
+    low_att2(n)=0;
+end
+k=k+5;
+n=n+1;
+end
+
+subplot(3,4,11)
+semilogx(low_att2,r22)
+
+title('Low Attinuation')
+xlabel('frequency')
+ylabel('magnitude')
+
+k=0.5; 
+high_att2=0;
+n=1;
+while(k<=3000)
+
+x2(n)= c2(n)/(a2(n)^2+b2(n));
+r22(n)=20*log10(abs(x2(n)));
+max_x2=max(r2);
+if(r22(n)<=max_x2-30 && r22(n)>=(min_x2))
+    
+   high_att2(n)=k;
+else
+    high_att2(n)=0;
+end
+k=k+5;
+n=n+1;
+end
+
+subplot(3,4,12)
+semilogx(high_att2,r22)
+
+title('high Attinuation')
+xlabel('frequency')
+ylabel('magnitude')
+
+%
